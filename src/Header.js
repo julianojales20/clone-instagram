66a61813b10f32ff9442b2dd980dad7f3e2676c3
Juliano Alonso <juliano_jales20@hotmail.com>
2024-11-05 11:09:53 -0300

    Final do Projeto

--- src/Header.js
+++ src/Header.js
@@ -1,133 +1,199 @@
-import { useEffect, useState } from "react";
+import {useEffect, useState} from 'react';
+import firebase from 'firebase';
+import {auth,storage,db} from './firebase.js';
+function Header(props){
 
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  updateProfile,
-  signInWithEmailAndPassword
-} from "firebase/auth";
+    const [progress,setProgress] = useState(0);
 
-export default function Header(props) {
-  useEffect(() => {}, []);
+    const [file, setFile] = useState(null);
 
-  const auth = getAuth();
+    useEffect(() => {
+         
+    }, [])
 
-  function criarConta(e) {
-    e.preventDefault();
-    let email = document.getElementById("email-cadastro").value;
-    let username = document.getElementById("username-cadastro").value;
-    let senha = document.getElementById("senha-cadastro").value;
 
-    // Criar conta no Firebase
-    createUserWithEmailAndPassword(auth, email, senha)
-      .then((userCredential) => {
-        const user = userCredential.user;
+    function criarConta(e){
+        
+        e.preventDefault();
+        let email = document.getElementById('email-cadastro').value;
+        let username = document.getElementById('username-cadastro').value;
+        let senha = document.getElementById('senha-cadastro').value;
 
-        // Atualizar o nome de usuário
-        updateProfile(user, {
-          displayName: username
+        //Criar conta firebase;
+        auth.createUserWithEmailAndPassword(email,senha)
+        .then((authUser)=>{
+            authUser.user.updateProfile({
+                displayName:username
+            })
+            alert('Conta criada com sucesso!');
+            let modal = document.querySelector('.modalCriarConta');
+
+              modal.style.display = "none";
+        }).catch((error)=>{
+          alert(error.message);
         })
-          .then(() => {
-            alert("Conta Criada com Sucesso");
-            let modal = document.querySelector(".modalCriarConta");
-            modal.style.display = "none";
+        ;
+
+
+    }
+
+
+    function logar(e){
+      e.preventDefault();
+      let email = document.getElementById('email-login').value;
+      let senha = document.getElementById('senha-login').value;
+
+      auth.signInWithEmailAndPassword(email,senha)
+      .then((auth)=>{
+        props.setUser(auth.user.displayName);
+        alert('Logado com sucesso!');
+        window.location.href = "/";
+      }).catch((err)=>{
+        alert(err.message);
+      })
+
+    }
+
+
+    function abrirModalCriarConta(e){
+      e.preventDefault();
+
+      let modal = document.querySelector('.modalCriarConta');
+
+      modal.style.display = "block";
+
+     
+    }
+
+    function abrirModalUpload(e){
+      e.preventDefault();
+
+      let modal = document.querySelector('.modalUpload');
+
+      modal.style.display = "block";
+    }
+
+    function fecharModalCriar(){
+      let modal = document.querySelector('.modalCriarConta');
+
+      modal.style.display = "none";
+    }
+
+    function fecharModalUpload(){
+      let modal = document.querySelector('.modalUpload');
+
+      modal.style.display = "none";
+    }
+
+    function deslogar(e){
+      e.preventDefault();
+      auth.signOut().then(function(val){
+          props.setUser(null);
+          window.location.href = "/";
+      })
+    }
+
+    function uploadPost(e){
+      e.preventDefault();
+      let tituloPost = document.getElementById('titulo-upload').value;
+      
+
+      const uploadTask = storage.ref(`images/${file.name}`).put(file);
+
+      uploadTask.on("state_changed",function(snapshot){
+          const progress = Math.round(snapshot.bytesTransferred/snapshot.totalBytes) * 100;
+          setProgress(progress);
+      },function(error){
+
+      }, function(){
+
+          storage.ref("images").child(file.name).getDownloadURL()
+          .then(function(url){
+              db.collection('posts').add({
+                titulo: tituloPost,
+                image: url,
+                userName: props.user,
+                timestamp: firebase.firestore.FieldValue.serverTimestamp()
+              })
+
+              setProgress(0);
+              setFile(null);
+
+              alert('Upload Realizado com sucesso!');
+
+              document.getElementById('form-upload').reset();
+              fecharModalUpload();
+
           })
-          .catch((error) => {
-            alert("Erro ao atualizar o perfil: " + error.message);
-          });
+
       })
-      .catch((error) => {
-        alert("Erro ao criar a conta: " + error.message);
-      });
-  }
 
-  function logar(e) {
-    e.preventDefault();
-    let email = document.getElementById("email-login").value;
-    let senha = document.getElementById("senha-login").value;
 
-    // Login no Firebase
-    signInWithEmailAndPassword(auth, email, senha)
-      .then((userCredential) => {
-        const user = userCredential.user;
-        // Atualizar a prop `setUser` corretamente
-        props.setUser(user.displayName);
-        alert("Login realizado com sucesso! Bem-vindo, " + user.displayName);
-      })
-      .catch((error) => {
-        alert("Erro ao fazer login: " + error.message);
-      });
-  }
+    }
 
-  function abrirModalCriarConta(e) {
-    e.preventDefault();
-    let modal = document.querySelector(".modalCriarConta");
+    return (
 
-    modal.style.display = "block";
-  }
+<div className="header">
 
-  function fecharModalCriar() {
-    let modal = document.querySelector(".modalCriarConta");
-
-    modal.style.display = "none";
-  }
-
-  return (
-    <div className="header">
       <div className="modalCriarConta">
-        <div className="formCriarConta">
-          <div onClick={() => fecharModalCriar()} className="close-modal-criar">
-            X
+          <div className="formCriarConta">
+            <div onClick={()=>fecharModalCriar()} className="close-modal-criar">X</div>
+              <h2>Criar Conta</h2>
+                <form onSubmit={(e)=>criarConta(e)}>
+                    <input id="email-cadastro" type="text" placeholder="Seu e-mail..." />
+                    <input id="username-cadastro" type="text" placeholder="Seu username..." />
+                    <input id="senha-cadastro" type="password" placeholder="Sua senha..." />
+                    <input type="submit" value="Criar Conta!" />
+                </form>
           </div>
-          <h2>Criar Conta</h2>
-          <form onSubmit={(e) => criarConta(e)}>
-            <input
-              id="email-cadastro"
-              type="text"
-              placeholder="Seu e-mail...."
-            />
-            <input
-              id="username-cadastro"
-              type="text"
-              placeholder="Seu username...."
-            />
-            <input
-              id="senha-cadastro"
-              type="password"
-              placeholder="Sua Senha...."
-            />
-            <input type="submit" value="Criar Conta" />
-          </form>
-        </div>
       </div>
+
+      <div className="modalUpload">
+          <div className="formUpload">
+            <div onClick={()=>fecharModalUpload()} className="close-modal-criar">X</div>
+              <h2>Fazer Upload</h2>
+                <form id="form-upload" onSubmit={(e)=>uploadPost(e)}>
+                    <progress id="progress-upload" value={progress}></progress>
+                    <input id="titulo-upload" type="text" placeholder="Nome da sua foto..." />
+                    <input onChange={(e)=>setFile(e.target.files[0])} type="file" name="file" />
+                    <input type="submit" value="Postar no instagram!" />
+                </form>
+          </div>
+      </div>
+
+
       <div className="center">
-        <div className="header__logo">
-          <a href="">
-            <img src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" />
-          </a>
-        </div>
-        {props.user ? (
-          <div className="header__logadoInfo">
-            <span>
-              Olá <b>{props.user}</b>
-            </span>
-            <a href="#">Postar!</a>
+          <div className="header__logo">
+              <a href=""><img src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" /></a>
           </div>
-        ) : (
-          <div className="header__loginForm">
-            <form onSubmit={(e) => logar(e)}>
-              <input id="email-login" type="text" placeholder="Login...." />
-              <input id="senha-login" type="password" placeholder="Senha...." />
-              <input type="submit" name="acao" value="Logar" />
-            </form>
-            <div className="btn__criarConta">
-              <a onClick={(e) => abrirModalCriarConta(e)} href="#">
-                Criar Conta!
-              </a>
+          {
+            (props.user)?
+            <div className="header__logadoInfo">
+              <span>Olá, <b>{props.user}</b></span>
+              <a onClick={(e)=>abrirModalUpload(e)} href="#">Postar!</a>
+              <a onClick={(e)=>deslogar(e)}>Deslogar</a>
             </div>
+            :
+            <div className="header__loginForm">
+              <form onSubmit={(e)=>logar(e)}>
+                <input id="email-login" type="text" placeholder="Login..." />
+                <input id="senha-login" type="password" placeholder="Senha..." />
+                <input type="submit" name="acao" value="Logar!" />
+              </form>
+              <div className="btn__criarConta">
+                <a onClick={(e)=>abrirModalCriarConta(e)} href="#">Criar Conta!</a>
+              </div>
           </div>
-        )}
-      </div>
-    </div>
-  );
+          }
+          
+          </div>
+</div>
+
+    )
+
+
 }
+
+
+
+export default Header;
--- src/firebase.js
+++ src/firebase.js
@@ -1,25 +1,12 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
-import { getStorage } from "firebase/storage";
-import { getFunctions } from "firebase/functions";
-import { getAnalytics } from "firebase/analytics";
+import firebase from 'firebase';
 
-const firebaseApp = initializeApp({
-  apiKey: "AIzaSyBxHm9YWZpp0aDsJAoIOjc7YQrWOh2UENI",
-  authDomain: "instagram-clone-curso-ad4b5.firebaseapp.com",
-  projectId: "instagram-clone-curso-ad4b5",
-  storageBucket: "instagram-clone-curso-ad4b5.appspot.com",
-  messagingSenderId: "773089308054",
-  appId: "1:773089308054:web:e1631df8ec7050648c81b7",
-  measurementId: "G-GBC63LPDTH"
-});
+const firebaseApp = firebase.initializeApp({
+    /*SUAS CREDENCIAIS*/
+  });
 
-// Iniciando os serviços com a abordagem modular
-const analytics = getAnalytics(firebaseApp);
-const db = getFirestore(firebaseApp);
-const auth = getAuth(firebaseApp);
-const storage = getStorage(firebaseApp);
-const functions = getFunctions(firebaseApp);
+const db = firebase.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage();
+const functions = firebase.functions();
 
-export { db, auth, storage, functions };
+export {db, auth, storage, functions};
